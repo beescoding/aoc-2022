706201e9b5ac0c04eb756e6023ff4851a653633b
beescoding <121716835+beescoding@users.noreply.github.com>
2023-01-01 11:24:05 +0000

    Update Day 19.py

--- Day 19.py
+++ Day 19.py
@@ -14,80 +14,68 @@ paths = []
 for k, line in enumerate(inp):
     blueprints.append([int(s) for s in re.findall(r'-?\d+\.?\d*', line[30:])]) # The blueprint number gets added if including first 30 characters
 
-class path:
-    def __init__(self, tupl):
-        self.minute = tupl[0]
-        self.goods = tupl[1]
-        self.robots = tupl[2]
-        self.blueprint = tupl[3]
-        self.best = tupl[4]
-        self.build = tupl[5]
+def path(minute, goods, robots, blueprint, best, build):
+    while minute <= 25:
+        if goods[3] + robots[3]*(25-minute) + ((24-minute)*(25-minute)/2) <= best:
+            return best
 
+        # deciding what robot to build (where recursion will take place)
+        if build[0] == 4:
+            #geode conditions
+            if robots[2]:
+                best = max(path(minute, goods.copy(), robots.copy(), blueprint, best, [3, False]), best)
+            #ore conditions
+            if minute < 22 and robots[0]*(22-minute) + goods[0] < (22-minute)*(max(blueprint[1], blueprint[2], blueprint[4])):
+                best = max(path(minute, goods.copy(), robots.copy(), blueprint, best, [0, False]), best)
+            #obsidian conditions
+            if robots[1] and minute < 22 and robots[2]*(22-minute) + goods[2] < (22-minute)*blueprint[5]:
+                best = max(path(minute, goods.copy(), robots.copy(), blueprint, best, [2, False]), best)
+            #clay conditions
+            if minute < 20 and robots[1]*(22-minute) + goods[1] < (22-minute)*blueprint[3]:
+                best = max(path(minute, goods.copy(), robots.copy(), blueprint, best, [1, False]), best)
+            return best
 
-    def robot(self):
-        while self.minute <= 25:
-            if self.goods[3] + self.robots[3]*(25-self.minute) + ((24-self.minute)*(25-self.minute)/2) <= self.best:
-                return self.best
+        #starting production
+        if (build[0] == 0 and goods[0] >= blueprint[0]) or (build[0] == 1 and goods[0] >= blueprint[1]) or\
+            (build[0] == 2 and goods[0] >= blueprint[2] and goods[1] >= blueprint[3]) or (build[0] == 3 and goods[0] >= blueprint[4] and goods[2] >= blueprint[5]):
+            build[1] = True
 
-            # deciding what robot to build (where recursion will take place)
-            if self.build[0] == 4:
-                #geode conditions
-                if self.robots[2]:
-                    self.best = max(path((self.minute, self.goods.copy(), self.robots.copy(), self.blueprint, self.best, [3, False])).robot(), self.best)
-                #ore conditions
-                if self.minute < 22 and self.robots[0]*(22-self.minute) + self.goods[0] < (22-self.minute)*(max(self.blueprint[1], self.blueprint[2], self.blueprint[4])):
-                    self.best = max(path((self.minute, self.goods.copy(), self.robots.copy(), self.blueprint, self.best, [0, False])).robot(), self.best)
-                #obsidian conditions
-                if self.robots[1] and self.minute < 22 and self.robots[2]*(22-self.minute) + self.goods[2] < (22-self.minute)*self.blueprint[5]:
-                    self.best = max(path((self.minute, self.goods.copy(), self.robots.copy(), self.blueprint, self.best, [2, False])).robot(), self.best)
-                #clay conditions
-                if self.minute < 20 and self.robots[1]*(22-self.minute) + self.goods[1] < (22-self.minute)*self.blueprint[3]:
-                    self.best = max(path((self.minute, self.goods.copy(), self.robots.copy(), self.blueprint, self.best, [1, False])).robot(), self.best)
-                return self.best
+        #collecting goods
+        for j in range(4):
+            goods[j] += robots[j]
 
-            #starting production
-            if (self.build[0] == 0 and self.goods[0] >= self.blueprint[0]) or (self.build[0] == 1 and self.goods[0] >= self.blueprint[1]) or\
-                (self.build[0] == 2 and self.goods[0] >= self.blueprint[2] and self.goods[1] >= self.blueprint[3]) or (self.build[0] == 3 and self.goods[0] >= self.blueprint[4] and self.goods[2] >= self.blueprint[5]):
-                self.build[1] = True
+        #robot finish (taking away goods and building robot)
+        if build[1]:
+            robots[build[0]] += 1
+            if build[0] == 3:
+                goods[0] -= blueprint[4]
+                goods[2] -= blueprint[5]
+            elif build[0] == 2:
+                goods[0] -= blueprint[2]
+                goods[1] -= blueprint[3]
+            else:
+                goods[0] -= blueprint[build[0]]
+            build = [4, False]
 
-            #collecting self.goods
-            for j in range(4):
-                self.goods[j] += self.robots[j]
-
-            #robot finish (taking away goods and building robot)
-            if self.build[1]:
-                self.robots[self.build[0]] += 1
-                if self.build[0] == 3:
-                    self.goods[0] -= self.blueprint[4]
-                    self.goods[2] -= self.blueprint[5]
-                elif self.build[0] == 2:
-                    self.goods[0] -= self.blueprint[2]
-                    self.goods[1] -= self.blueprint[3]
-                else:
-                    self.goods[0] -= self.blueprint[self.build[0]]
-                self.build = [4, False]
-
-            #next minuteute
-            self.minute += 1
-            if self.minute == 24:
-                self.best = max(self.best, self.goods[3] + self.robots[3])
-                return self.best
+        #next minuteute
+        minute += 1
+        if minute == 24:
+            best = max(best, goods[3] + robots[3])
+            return best
 
 
 for i in range(len(blueprints)):
     robots = [1, 0, 0, 0]
     goods = [0, 0, 0, 0]
-    top = path((1, goods, robots, blueprints[i], 0, [4, False])).robot()
-    print(i+1, top)
+    top = path(1, goods, robots, blueprints[i], 0, [4, False])
     quality += (i + 1) * top
-print(quality)
+print("Part 1", quality)
 print("Part 1 took", time.time() - start_time, "to run")
 quality = 1
 for i in range(3):
     robots = [1, 0, 0, 0]
     goods = [0, 0, 0, 0]
-    top = path((-7, goods, robots, blueprints[i], 0, [4, False])).robot()
-    print(i+1, top)
+    top = path(-7, goods, robots, blueprints[i], 0, [4, False])
     quality *= top
-print(quality)
+print("Part 2", quality)
 print("Part 1+2 took", time.time() - start_time, "to run")
